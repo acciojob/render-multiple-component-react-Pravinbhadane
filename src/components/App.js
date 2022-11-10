884dc141ef76a981d08cb2bd02a137512105eeba
Pravin Bhadane <pravinbhadane60@gmail.com>
2022-11-10 20:56:19 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+       <h3 data-ns-test="project-name">E-commerce Project</h3>
+      <p data-ns-test='project-description'>I want to built a e-commerce react app.</p>
+    </div>
   )
 }
 
